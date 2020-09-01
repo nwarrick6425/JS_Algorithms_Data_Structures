8b6e180e988b676a62923b81a34188633fb24dab
Nicholai Dudakiw-Warrick <nwarrick6425@gmail.com>
2020-08-31 23:26:46 -0500

    Refactor binarySearch func - made clear and concise

--- Searching_Algoritms/Binary_Search/app.js
+++ Searching_Algoritms/Binary_Search/app.js
@@ -1,20 +1,13 @@
 function binarySearch(arr, val) {
-  let start = 0
+  let start = 0;
   let end = arr.length - 1;
-  let middle = 0;
+  let middle = Math.floor((start + end) / 2);
 
-  while (start < end) {
-    middle = Math.floor(end - start / 2);
-    if (arr[middle] === val) {
-      return middle;
-    }
-    else if (val < arr[middle]) {
-      end = middle - 1;
-    }
-    else if (val > arr[middle]) {
-      start = middle + 1;
-    }
+  while (arr[middle] !== val && start <= end) {
+    if (val < arr[middle]) end = middle - 1;
+    else start = middle + 1;
+    middle = Math.floor((start + end) / 2);
   }
 
-  return -1;
+  return arr[middle] === val ? middle : -1;
 }
