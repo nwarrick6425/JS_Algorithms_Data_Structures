e98c611e08277fab57ea95b2d8ee72121b585498
Nicholai Dudakiw-Warrick <nwarrick6425@gmail.com>
2020-09-13 23:24:26 -0500

    Add pivot function for quick sort algo

---
+++ Sorting_Algorithms/Quick_Sort/app.js
@@ -0,0 +1,21 @@
+function pivot(arr, start=0, end=arr.length - 1) {
+  function swap(arr, i, j) {
+    let temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+  }
+
+  let pivot = arr[start];
+  let swapIdx = 0;
+
+  for (let i = start + 1; i < arr.length; i++) {
+    if (pivot > arr[i]) {
+      swapIdx++;
+      swap(arr, swapIdx, i);
+    }
+  }
+
+  swap(arr, start, swapIdx);
+
+  return swapIdx;
+}
