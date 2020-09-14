a5e784299a7e89e9b8557cc3e5d9edeedfc859af
Nicholai Dudakiw-Warrick <nwarrick6425@gmail.com>
2020-09-13 22:37:35 -0500

    Add mergeSort functionality and .gitignore file

--- Sorting_Algorithms/Merge_Sort/app.js
+++ Sorting_Algorithms/Merge_Sort/app.js
@@ -1,7 +1,3 @@
-function mergeSort(arr) {
-  return arr;
-}
-
 function merge(arr1, arr2) {
   let i = 0;
   let j = 0;
@@ -23,7 +19,7 @@ function merge(arr1, arr2) {
     i++;
   }
 
-  while (j < arr2.legnth) {
+  while (j < arr2.length) {
     newArr.push(arr2[j]);
     j++;
   }
@@ -31,4 +27,12 @@ function merge(arr1, arr2) {
   return newArr;
 }
 
-console.log(merge([1,3,5,7,9,10,11], [2,4,6,8]));
+function mergeSort(arr) {
+  if (arr.length <= 1) return arr;
+  let mid = Math.floor(arr.length / 2);
+  let left = mergeSort(arr.slice(0, mid));
+  let right = mergeSort(arr.slice(mid));
+  return merge(left, right);
+}
+
+console.log(mergeSort([23,5,33,111,65,3,3,4,5,90]));
