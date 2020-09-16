cd33c8185d54c82036aa60963b72d76753ed4546
Nicholai Dudakiw-Warrick <nwarrick6425@gmail.com>
2020-09-15 23:04:14 -0500

    Add digitCount helper - counts number of digits in number

--- Sorting_Algorithms/Radix_Sort/app.js
+++ Sorting_Algorithms/Radix_Sort/app.js
@@ -1,3 +1,8 @@
 function getDigit(num, i) {
   return Math.floor(Math.abs(num) / Math.pow(10, i)) % 10;
 }
+
+function digitCount(num) {
+  if (num === 0) return 1;
+  return Math.floor(Math.log10(Math.abs(num))) + 1;
+}
