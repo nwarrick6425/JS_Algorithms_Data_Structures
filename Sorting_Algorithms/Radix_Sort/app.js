29c6f2045b6ba6a123ea760a45232a2e55acecba
Nicholai Dudakiw-Warrick <nwarrick6425@gmail.com>
2020-09-15 23:19:11 -0500

    Add mostDigits helper - returns the longest number of digits in array

--- Sorting_Algorithms/Radix_Sort/app.js
+++ Sorting_Algorithms/Radix_Sort/app.js
@@ -6,3 +6,11 @@ function digitCount(num) {
   if (num === 0) return 1;
   return Math.floor(Math.log10(Math.abs(num))) + 1;
 }
+
+function mostDigits(arr) {
+  let max = 0;
+  for (let num of arr) {
+    max = Math.max(max, digitCount(num));
+  }
+  return max;
+}
