2a13cb8b3927d02612214654da1d8e528b69af95
Nicholai Dudakiw-Warrick <nwarrick6425@gmail.com>
2020-09-15 22:36:47 -0500

    Add quickSort function logic

--- Sorting_Algorithms/Quick_Sort/app.js
+++ Sorting_Algorithms/Quick_Sort/app.js
@@ -6,7 +6,7 @@ function pivot(arr, start=0, end=arr.length - 1) {
   }
 
   let pivot = arr[start];
-  let swapIdx = 0;
+  let swapIdx = start;
 
   for (let i = start + 1; i < arr.length; i++) {
     if (pivot > arr[i]) {
@@ -19,3 +19,14 @@ function pivot(arr, start=0, end=arr.length - 1) {
 
   return swapIdx;
 }
+
+function quickSort (arr, left = 0, right = arr.length - 1) {
+  if (left < right) {
+    let pivotIndex = pivot(arr, left, right);
+    quickSort(arr, left, pivotIndex - 1);
+    quickSort(arr, pivotIndex + 1, right);
+  }
+  return arr;
+}
+
+console.log(quickSort([3,21,5,3,66,7,8,3,-1,-55,-3]));
