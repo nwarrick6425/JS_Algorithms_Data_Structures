00c685ecf834f945036a95fe3ea712dd53743416
Nicholai Dudakiw-Warrick <nwarrick6425@gmail.com>
2020-09-24 22:29:00 -0500

    Refactor #pop() tests

--- test/singlyLinkedList.js
+++ test/singlyLinkedList.js
@@ -1,50 +1,61 @@
 const SinglyLinkedList = require('../Data_Structures/Singly_Linked_List/singlyLinkedList')
 let expect = require('chai').expect;
-let list;
+let list, curTail, curLength, poppedTail;
+
+describe('#push()', function() {
+  
+  context('with no items', function() {
+    beforeEach(function() {
+      list = new SinglyLinkedList();
+    })
+
+    it('should', function() {
+
+    })
+  })
+})
 
 describe('#pop()', function() {
   
   context('with many items', function() {
-    beforeEach(function () {
+    before(function () {
       list = new SinglyLinkedList().push(1).push(2).push(3);
+      curTail = list.tail;
+      curLength = list.length;
+      poppedTail = list.pop();
     })
 
     it('should return the tail', () => {
-      let curTail = list.tail;
-      expect(list.pop()).to.equal(curTail);
+      expect(curTail).to.equal(poppedTail);
     })
 
     it('should decrease the length by 1', function () {
-      let curLength = list.length;
-      list.pop();
       expect(list.length).to.equal(curLength - 1);
     })
 
   })
 
   context('with one item', function() {
-    beforeEach(function () {
+    before(function () {
       list = new SinglyLinkedList().push(1);
+      list.pop();
     })
 
     it('should set head to null', function() {
-      list.pop();
       expect(list.head).to.equal(null);
     })
 
     it('should set tail to null', function() {
-      list.pop();
       expect(list.tail).to.equal(null);
     })
 
     it('should make the list length 0', function() {
-      list.pop();
       expect(list.length).to.equal(0);
     })
   })
 
   context('with no items', function() {
-    beforeEach(function () {
+    before(function () {
       list = new SinglyLinkedList();
     })
 
