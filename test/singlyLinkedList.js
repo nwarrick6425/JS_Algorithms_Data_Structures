c7a80c89b484e197d045564426c53f9e34dce24a
Nicholai Dudakiw-Warrick <nwarrick6425@gmail.com>
2020-09-24 22:17:56 -0500

    Add tests for #pop()

--- test/singlyLinkedList.js
+++ test/singlyLinkedList.js
@@ -3,14 +3,28 @@ let expect = require('chai').expect;
 let list;
 
 describe('#pop()', function() {
-  beforeEach( function() {
-    list = new SinglyLinkedList().push(1);
-  })
   
-  context('with one item', function() {
+  context('with many items', function() {
+    beforeEach(function () {
+      list = new SinglyLinkedList().push(1).push(2).push(3);
+    })
+
     it('should return the tail', () => {
       let curTail = list.tail;
       expect(list.pop()).to.equal(curTail);
+    })
+
+    it('should decrease the length by 1', function () {
+      let curLength = list.length;
+      list.pop();
+      expect(list.length).to.equal(curLength - 1);
+    })
+
+  })
+
+  context('with one item', function() {
+    beforeEach(function () {
+      list = new SinglyLinkedList().push(1);
     })
 
     it('should set head to null', function() {
@@ -23,15 +37,19 @@ describe('#pop()', function() {
       expect(list.tail).to.equal(null);
     })
 
-    it('should decrease the length by 1', function() {
-      let curLength = list.length;
-      list.pop();
-      expect(list.length).to.equal(curLength - 1);
-    })
-
     it('should make the list length 0', function() {
       list.pop();
       expect(list.length).to.equal(0);
+    })
+  })
+
+  context('with no items', function() {
+    beforeEach(function () {
+      list = new SinglyLinkedList();
+    })
+
+    it('should return undefined', function() {
+      expect(list.pop()).to.equal(undefined);
     })
   })
 })
