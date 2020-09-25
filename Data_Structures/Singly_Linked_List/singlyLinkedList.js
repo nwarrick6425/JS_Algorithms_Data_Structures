86eb94784029138dd6991fdbee407c612a418843
Nicholai Dudakiw-Warrick <nwarrick6425@gmail.com>
2020-09-24 23:41:23 -0500

    Add #insert() - Inserts new Node at specified index

--- Data_Structures/Singly_Linked_List/singlyLinkedList.js
+++ Data_Structures/Singly_Linked_List/singlyLinkedList.js
@@ -112,6 +112,21 @@ class SinglyLinkedList {
 
     return false;
   }
+
+  insert(index, val) {
+    if (index < 0 || index > this.length) return false;
+    if (index === this.length) return !!this.push(val);
+    if (index === 0) return !!this.unshift(val);
+    
+    let newNode = new Node(val);
+    let prev = this.get(index - 1);
+    newNode.next = prev.next;
+    prev.next = newNode;
+    this.length++;
+
+    return true;
+
+  }
 }
 
 module.exports = SinglyLinkedList;
